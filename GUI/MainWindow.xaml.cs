330d161da227c3e671f1df51fdd8f8bfd274074b
Dario Zela <dariozela1@gmail.com>
2019-12-21 11:23:09 +0000

    Testing the map generation

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Models.World_Gen;
 
 namespace GUI
 {
@@ -20,9 +22,41 @@ namespace GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        Image Image;
+
         public MainWindow()
         {
+            Set();
             InitializeComponent();
+            GameBoard.Children.Add(Image);
+        }
+
+        private WriteableBitmap CreateImage(double[,] imageData, int Height, int Width)
+        {
+            WriteableBitmap image = new WriteableBitmap(Width, Height, 1, 1, PixelFormats.Gray32Float, null);
+            image.WritePixels(new Int32Rect(0,0, Width, Height), imageData, (Width * image.Format.BitsPerPixel + 7) / 8, 0);
+            return image;
+        }
+
+        public void Set(double scope = 10)
+        {
+            int mapWidth = 700;
+            int mapDepth = 700;
+
+            MapGen map = new MapGen();
+            double[,] world = map.GenerateNoiseMap(mapDepth, mapWidth, scope);
+            Image image = new Image();
+            image.Source = CreateImage(world, mapDepth, mapWidth);
+            image.Width = 700;
+            image.Height = 700;
+            Image = image;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Set(Slider.Value);
+            GameBoard.Children.Remove(Image);
+            GameBoard.Children.Add(Image);
         }
     }
 }
