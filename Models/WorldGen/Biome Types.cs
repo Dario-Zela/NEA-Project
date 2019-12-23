ee0dfbea5527dc5f139ab6a4ff42fd0ce0837ff3
Dario Zela <dariozela1@gmail.com>
2019-12-23 09:58:00 +0000

    Initial biome setup algorithm

--- Models/WorldGen/Biome Types.cs
+++ Models/WorldGen/Biome Types.cs
@@ -4,256 +4,184 @@ using System.Text;
 
 namespace Models.WorldGen
 {
-    struct Biome
-    {
-        public abstract Uri TexturePack { get; }
-        public abstract float minHeight { get; }
-        public abstract float maxHeight { get; }
-        public abstract float minHumidityLevel { get; }
-        public abstract float maxHumidityLevel { get; }
-        public abstract float minTemperatureLevel { get; }
-        public abstract float maxTemperatureLevel { get; }
-        public abstract float daylyTempChangeRange { get; }
-        public abstract float seasonTempChangeRange { get; }
-        public abstract float daylyHumChangeRange { get; }
-        public abstract float seasonHumChangeRange { get; }
-        public abstract float treeFrequency { get; }
-        public abstract float riversFrequency { get; }
-        public abstract float lakeFrequency { get; }
-        public abstract bool canHaveWaterSources { get; }
-        public abstract float bushFrequency { get; }
-        public const int Size = 250;
-        public abstract bool isValid();
-    }
-
-    struct Sea : Biome
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-
-        public override float minHeight => 0f;
-
-        public override float maxHeight => 0.1f;
-
-        public override float minHumidityLevel => 0f;
-
-        public override float maxHumidityLevel => 100f;
-
-        public override float minTemperatureLevel => 0f;
-
-        public override float maxTemperatureLevel => 30f;
-
-        public override float daylyTempChangeRange => 20f;
-
-        public override float seasonTempChangeRange => 10f;
-
-        public override float daylyHumChangeRange => 50f;
-
-        public override float seasonHumChangeRange => 25f;
-
-        public override float treeFrequency => 0;
-
-        public override float riversFrequency => 0f;
-
-        public override float lakeFrequency => 0f;
-
-        public override bool canHaveWaterSources => false;
-
-        public override float bushFrequency => 0f;
-    }
-
-    struct FrozenSea : Sea
-    {
-        public override float minTemperatureLevel => -10f;
-        public override float maxTemperatureLevel => 0f;
-        public override Uri TexturePack => throw new NotImplementedException();
-    }
-
-    struct Swamp : Biome
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-
-        public override float minHeight => 0.08f;
-
-        public override float maxHeight => 0.13f;
-
-        public override float minHumidityLevel => 75f;
-
-        public override float maxHumidityLevel => 100f;
-
-        public override float minTemperatureLevel => 10f;
-
-        public override float maxTemperatureLevel => 30f;
-
-        public override float daylyTempChangeRange => 5f;
-
-        public override float seasonTempChangeRange => 5f;
-
-        public override float daylyHumChangeRange => 10f;
-
-        public override float seasonHumChangeRange => 5f;
-
-        public override float treeFrequency => 45f;
-
-        public override float riversFrequency => 50f;
-
-        public override float lakeFrequency => 33f;
-
-        public override bool canHaveWaterSources => false;
-
-        public override float bushFrequency => 10f;
-    }
-
-    struct Sand : Biome
-    {
-
-    }
-
-    struct Plain : Biome
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-
-        public override float minHeight => 0.2f;
-
-        public override float maxHeight => 0.6f;
-
-        public override float minHumidityLevel => 20f;
-
-        public override float maxHumidityLevel => 60f;
-
-        public override float minTemperatureLevel => 10f;
-
-        public override float maxTemperatureLevel => 20f;
-
-        public override float daylyTempChangeRange => 10f;
-
-        public override float seasonTempChangeRange => 15f;
-
-        public override float daylyHumChangeRange => 15f;
-
-        public override float seasonHumChangeRange => 10f;
-
-        public override float treeFrequency => 10f;
-
-        public override float riversFrequency => 10f;
-
-        public override float lakeFrequency => 10f;
-
-        public override bool canHaveWaterSources => false;
-
-        public override float bushFrequency => 30f;
-    }
-
-    struct FrozenPlain : Plain
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-        public override float maxTemperatureLevel => 10f;
-
-        public override float minTemperatureLevel => 0f;
-
-    }
-
-    struct Savahna : Plain
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-        public override float maxTemperatureLevel => 30f;
-
-        public override float minTemperatureLevel => 20f;
-
-        public override float minHumidityLevel => 0;
-
-        public override float maxHumidityLevel => 20;
-
-        public override float treeFrequency => 5f;
-    }
-
-    struct Hills : Biome
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-        public override float minHeight => 0.6f;
-
-        public override float maxHeight => 0.7f;
-    }
-
-    struct ForestHill : Hills
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-        public override float treeFrequency => 45f;
-        public override float bushFrequency => 35f;
-    }
-
-    struct Forest : Plain
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-        public override float bushFrequency => 35f;
-        public override float treeFrequency => 45f;
-    }
-
-    struct Mountains : Biome
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-
-        public override float minHeight => throw new NotImplementedException();
-
-        public override float maxHeight => throw new NotImplementedException();
-
-        public override float minHumidityLevel => throw new NotImplementedException();
-
-        public override float maxHumidityLevel => throw new NotImplementedException();
-
-        public override float minTemperatureLevel => throw new NotImplementedException();
-
-        public override float maxTemperatureLevel => throw new NotImplementedException();
-
-        public override float daylyTempChangeRange => throw new NotImplementedException();
-
-        public override float seasonTempChangeRange => throw new NotImplementedException();
-
-        public override float daylyHumChangeRange => throw new NotImplementedException();
-
-        public override float seasonHumChangeRange => throw new NotImplementedException();
-
-        public override float treeFrequency => throw new NotImplementedException();
-
-        public override float riversFrequency => throw new NotImplementedException();
-
-        public override float lakeFrequency => throw new NotImplementedException();
-
-        public override bool canHaveWaterSources => throw new NotImplementedException();
-
-        public override float bushFrequency => throw new NotImplementedException();
-    }
-
-    struct Peak : Mountains
-    {
-        public override Uri TexturePack => throw new NotImplementedException();
-        public override float minHeight => 0.9f;
-        public override float maxHeight => 1f;
-
-    }
-
-    struct SnowyPeak : Peak
-    {
-
-    }
-
-    struct SavahnaHill : Hills
-    {
-
-    }
-
-    struct SnowyHill : Hills
-    {
-
-    }
-
-    struct ForestMountains : Mountains
-    {
-
-    }
-
     public class GetBiome
     {
+        private int mapWidth;
+        private int mapHeight;
+        private float[,] heightMap;
+        private float[,] humidityMap;
+        private float[,] temperatureMap;
+        private readonly int[,] IdMap;
 
+        public GetBiome(float[,] heightMap, float[,] humidityMap, float[,] temperatureMap, int mapWidth, int mapHeight)
+        {
+            this.heightMap = heightMap;
+            this.humidityMap = humidityMap;
+            this.temperatureMap = temperatureMap;
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            IdMap = ReturnBioms();
+        }
+
+        private void CheckHeight(ref int[,] IdMap)
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int j = 0; j < mapHeight; j++)
+                {
+                    if (heightMap[i, j] < 0.1f)
+                    {
+                        IdMap[i, j] = 1;
+                    }
+                    else if (heightMap[i, j] < 0.15f)
+                    {
+                        IdMap[i, j] = 2;
+                    }
+                    else if (heightMap[i, j] < 0.6f)
+                    {
+                        IdMap[i, j] = 3;
+                    }
+                    else if(heightMap[i, j] < 0.7)
+                    {
+                        IdMap[i, j] = 4;
+                    }
+                    else if (heightMap[i, j] < 0.9)
+                    {
+                        IdMap[i, j] = 5;
+                    }
+                    else if (heightMap[i, j] < 1)
+                    {
+                        IdMap[i, j] = 6;
+                    }
+                }
+            }
+        }
+
+        private void AssignBioms(ref int[,] idMap)
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                for (int j = 0; j < mapHeight; j++)
+                {
+                    if (idMap[i,j] == 1)
+                    {
+                        if (temperatureMap[i, j] < 0.4f)
+                        {
+                            IdMap[i, j] = 11;
+                        }
+                        else
+                        {
+                            IdMap[i, j] = 10;
+                        }
+                    }
+                    if (idMap[i, j] == 2)
+                    {
+                        if (temperatureMap[i, j] < 0.4f)
+                        {
+                            IdMap[i, j] = 21;
+                        }
+                        else
+                        {
+                            IdMap[i, j] = 20;
+                        }
+                    }
+                    else if (idMap[i, j] == 3)
+                    {
+                        if (temperatureMap[i, j] < 0.4f)
+                        {
+                            if (humidityMap[i, j] < 0.4f)
+                            {
+                                IdMap[i, j] = 31;
+                            }
+                            else
+                            {
+                                IdMap[i, j] = 32;
+                            }
+                        }
+                        else
+                        {
+                            if (humidityMap[i, j] < 0.4f)
+                            {
+                                IdMap[i, j] = 30;
+                            }
+                            else
+                            {
+                                IdMap[i, j] = 33;
+                            }
+                        }
+                    }
+                    else if (idMap[i, j] == 4)
+                    {
+                        if (temperatureMap[i, j] < 0.4f)
+                        {
+                            if (humidityMap[i, j] < 0.4f)
+                            {
+                                IdMap[i, j] = 41;
+                            }
+                            else
+                            {
+                                IdMap[i, j] = 42;
+                            }
+                        }
+                        else
+                        {
+                            if (humidityMap[i, j] < 0.4f)
+                            {
+                                IdMap[i, j] = 43;
+                            }
+                            else
+                            {
+                                IdMap[i, j] = 40;
+                            }
+                        }
+                    }
+                    else if (idMap[i, j] == 5)
+                    {
+                        if (temperatureMap[i, j] < 0.4f)
+                        {
+                            if (humidityMap[i, j] < 0.4f)
+                            {
+                                IdMap[i, j] = 50;
+                            }
+                            else
+                            {
+                                IdMap[i, j] = 51;
+                            }
+                        }
+                        else
+                        {
+                            IdMap[i, j] = 50;
+                        }
+                    }
+                    else if (idMap[i, j] == 6)
+                    {
+                        if (temperatureMap[i, j] < 0.4f)
+                        {
+                            if (humidityMap[i, j] < 0.4f)
+                            {
+                                IdMap[i, j] = 60;
+                            }
+                            else
+                            {
+                                IdMap[i, j] = 61;
+                            }
+                        }
+                        else
+                        {
+                            IdMap[i, j] = 60;
+                        }
+                    }
+                }
+            }
+        }
+
+        private int[,] ReturnBioms()
+        {
+            int[,] IdMapTemp = new int[heightMap.Rank, heightMap.Length];
+            CheckHeight(ref IdMapTemp);
+            AssignBioms(ref IdMapTemp);
+            return IdMapTemp;
+        }
     }
 
 }
