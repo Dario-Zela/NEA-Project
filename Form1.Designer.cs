01d68bfda9f4267b58375a59371097abebeea35f
Dario-Zela <58654337+Dario-Zela@users.noreply.github.com>
2020-01-24 12:57:45 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿using SharpGL;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Pixel_Engine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Controls.Clear();
+            this.glControl = new OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.glControl)).BeginInit();
+            this.SuspendLayout();
+            this.glControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl.FrameRate = 20;
+            this.glControl.Name = "GLControl";
+            this.glControl.TabIndex = 0;
+            this.glControl.RenderContextType = RenderContextType.NativeWindow;
+
+            this.Controls.Add(glControl);
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+            this.KeyPreview = true;
+        }
+
+        public OpenGLControl GetGLControl()
+        {
+            return glControl;
+        }
+
+        private OpenGLControl glControl;
+        #endregion
+    }
+}
+
