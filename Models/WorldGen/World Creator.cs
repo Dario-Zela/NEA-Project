cd1c4f55c3d01539ed322067eaeff452cb5b94f7
Dario-Zela <58654337+Dario-Zela@users.noreply.github.com>
2020-03-03 12:59:59 +0000

    Removing Colour to reduce casting time

--- Models/WorldGen/World Creator.cs
+++ Models/WorldGen/World Creator.cs
@@ -8,54 +8,6 @@ using Models.Sprites;
 
 namespace Models.WorldGen
 {
-    public class Colour
-    {
-        public Colour(byte R, byte G, byte B)
-        {
-            r = R;
-            g = G;
-            b = B;
-        }
-
-        public byte r;
-        public byte g;
-        public byte b;
-
-        public static bool operator ==(Colour A, Colour B)
-        {
-            return A.r == B.r && A.g == B.g && A.b == B.b;
-        }
-
-        public static bool operator !=(Colour A, Colour B)
-        {
-            return !(A == B);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Colour;
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = -839137856;
-            hashCode = hashCode * -1521134295 + r.GetHashCode();
-            hashCode = hashCode * -1521134295 + g.GetHashCode();
-            hashCode = hashCode * -1521134295 + b.GetHashCode();
-            return hashCode;
-        }
-
-        static public implicit operator Pixel(Colour c)
-        {
-            return new Pixel(c.r, c.g, c.b);
-        }
-
-        static public implicit operator Colour(Pixel c)
-        {
-            return new Colour(c.R, c.G, c.B);
-        }
-    };
-
     public class WorldCreator
     {
         //public GetBiome biomeMap;
