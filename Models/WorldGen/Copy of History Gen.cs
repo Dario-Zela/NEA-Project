bc1bf2a5e8a462cec9b5a70f125a15f8c79b11b9
Dario-Zela <58654337+Dario-Zela@users.noreply.github.com>
2020-03-11 10:09:06 +0000

    Update Copy of History Gen.cs

--- Models/WorldGen/Copy of History Gen.cs
+++ Models/WorldGen/Copy of History Gen.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
-using Pixel_Engine;
 using System.ComponentModel;
+using Models.WorldGen;
 
-namespace Models.WorldGen
+namespace Models.WorldGens
 {
-    class Region
+    class RegionModifier
     {
-        public Position Position;
         public int Modifier;
+
+        static public explicit operator RegionModifier(Terrain t)
+        {
+            return new RegionModifier() { Modifier = (int)((t.height * 10 + t.rainfall * 20 + t.temperature * 10) / 100f) };
+        }
+
+        public void addStructurModifier(Structure structure)
+        {
+            Modifier += structure.DefenceMod;
+        }
     }
 
     class UnitType
@@ -20,11 +29,22 @@ namespace Models.WorldGen
         public int OffenciveBonus;
         public int BaseMobility;
         public bool OutDated;
+        public int Cost;
+
+        public UnitType(int DefenciveBonus, int OffenciveBonus, int BaseMobility, int Cost)
+        {
+            this.BaseMobility = BaseMobility;
+            this.DefenciveBonus = DefenciveBonus;
+            this.OffenciveBonus = OffenciveBonus;
+            this.Cost = Cost;
+            OutDated = false;
+        }
     }
 
     class Unit
     {
-        public Region CurrentPosition;
+        public Position Position;
+        public RegionModifier CurrentPosition;
         int DefenciveBonus;
         int OffenciveBonus;
         int BaseMobility;
@@ -33,8 +53,9 @@ namespace Models.WorldGen
         int RemainingUnits;
         int Mobility;
 
-        public Unit(UnitType Division)
+        public Unit(UnitType Division, Position pos)
         {
+            Position = pos;
             DefenciveBonus = Division.DefenciveBonus;
             OffenciveBonus = Division.OffenciveBonus;
             BaseMobility = Division.BaseMobility;
@@ -54,31 +75,34 @@ namespace Models.WorldGen
             Supplies += Value;
         }
 
-        static public void operator +(Unit Attacker, Unit Defender)
+        static public bool operator +(Unit Attacker, Unit Defender)
         {
             if (Attacker.OffenciveBonus > Defender.DefenciveBonus)
             {
                 int BaseDamage = (Attacker.OffenciveBonus - Defender.DefenciveBonus) * new Random().Next(8);
                 Defender.RemainingUnits = (int)(BaseDamage - (float)(Defender.Mobility + Defender.Supplies + Defender.Experience) / 100);
                 Defender.Supplies -= BaseDamage / 10;
+                return true;
             }
             else
             {
                 int BaseDamage = (Attacker.OffenciveBonus - Defender.DefenciveBonus) * new Random().Next(8);
                 Attacker.RemainingUnits = (int)(BaseDamage - (float)(Attacker.Supplies + Attacker.Experience) / 100);
+                Attacker.Supplies -= BaseDamage / 10;
+                return false;
             }
         }
 
-        public void Moved(Region NewPosition)
+        public void Moved(RegionModifier NewPosition)
         {
             CurrentPosition = NewPosition;
             Mobility = BaseMobility - CurrentPosition.Modifier;
         }
     }
 
-    class Structures
+    class Structure
     {
-
+        public int DefenceMod;
     }
 
     struct Position
@@ -103,21 +127,26 @@ namespace Models.WorldGen
         public int[] Ai;
         public List<Unit> Army;
         public List<UnitType> PossibleDivisions;
-        public List<Structures> Buildings;
+        public List<Structure> Buildings;
         public int ResourcesPerTurn;
+        public int SupplyGen;
+        public List<Civilization> Allies;
         public Position Capital;
 
         public Civilization()
         {
+            PossibleDivisions = new List<UnitType>();
             TechLevel = new int[3];
             CivTag = "";
             Race = new Specie();
             Ai = new int[3];
             Army = new List<Unit>();
-            Buildings = new List<Structures>();
+            Buildings = new List<Structure>();
             ResourcesPerTurn = 0;
             Capital = new Position();
         }
+
+        public A
     }
 
     class Specie
